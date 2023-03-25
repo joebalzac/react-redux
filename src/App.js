15f6d361261b9be5f5176617588ec0433664ce9d
joebalzac <jgbalzac@gmail.com>
2023-03-25 09:01:39 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const items = [1, 2, 3, 4];
+
   return (
     <div className="App">
       <header className="App-header">
