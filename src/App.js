459afe5a1f3a5bd65c80e8f5c569907953eb259c
joebalzac <jgbalzac@gmail.com>
2023-03-30 17:47:11 -0400

    Welcome home

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ function App() {
   const name = "Big Pun";
   const message = "I am going to be a software engineer";
 
+  // Create Functions
+  const sayHelloHandler = (user) => {
+    console.log(`Hello there ${user}`)
+  }
+
   return (
     <div className="App">
       <h1>Software Engineer</h1>
-      <CreateTweet author="Lloyd Banks" title="The Man and the legend" />
-      <TweetList name={name} message={message} />
+      {/* <CreateTweet author="Lloyd Banks" title="The Man and the legend" />
+      <TweetList name={name} message={message} /> */}
       <h1>{name}</h1>
+      <button onClick={sayHelloHandler('JOEY B')}>Click</button>
     </div>
   );
 }
